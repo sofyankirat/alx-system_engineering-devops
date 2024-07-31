86a4ea285ef65d08776c809356fd1e411632ae66
sofyankirat <www.sofyankirat123@gmail.com>
2024-07-30 19:31:31 -0700

    kk

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,36 +1,43 @@
 #!/usr/bin/python3
-"""
-A Python script that, using this REST API, for a given employee ID,
-returns information about his/her TODO list progress.
-"""
+"""A simple module to make API Calls
+To a mockup API server and return the
+Responses. Then print them out to standard output
+Usage: ./0-gather-data_from_an_API <ID>
+Where <ID> is the employee ID for whom we want to list
+The tasks"""
 
-import requests
+from requests import get
 from sys import argv
 
+headers = {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+    "User-Agent": "Thing Gecko/20100101 Firefox/102.0"
+}
+base_url = "https://jsonplaceholder.typicode.com/users/"
 
-if __name__ == '__main__':
-    eid = argv[1]
-    userUrl = "https://jsonplaceholder.typicode.com/users"
-    url = userUrl + "/" + eid
 
-    response = requests.get(url)
-    EMPLOYEE_NAME = response.json().get('name')
+def get_task_status(user_id: str) -> None:
+    """
+    Get the task status for a certain user
+    Args:
+        user_id (str): The user id of the user
+    """
+    # lets first get the name of Employee
+    emp_name = get("{}{}".format(base_url, user_id)).json().get("name")
+    full_url = "{}{}/todos/".format(base_url, user_id)
+    response = get(full_url, headers=headers).json()
+    # lets get the total number of tasks shall we?
+    total_tasks = len(response)
 
-    todoUrl = url + "/todos"
-    response = requests.get(todoUrl)
-    tasks = response.json()
-    totalTasks = len(tasks)
+    # How about done tasks
+    done_tasks = [task['title'] for task in response
+                  if task['completed']]
+    done_tasks_count = len(done_tasks)
+    print("Employee {} is done with tasks({}/{}):".format(
+        emp_name, done_tasks_count, total_tasks))
+    [print("\t {}".format(task)) for task in done_tasks]
 
-    NUMBER_OF_DONE_TASKS = 0
-    tasksList = []
 
-    for task in tasks:
-        if task.get('completed'):
-            tasksList.append(task)
-            NUMBER_OF_DONE_TASKS += 1
-
-    print("Employee {} is done with tasks({}/{}):"
-          .format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, totalTasks))
-
-    for task in tasksList:
-        print("\t {}".format(task.get('title')))
+if __name__ == "__main__":
+    get_task_status(argv[1])
