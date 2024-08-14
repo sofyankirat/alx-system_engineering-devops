5ce3fab509899e4c8b4b04c4829d2875702de2c5
root <www.sofyankirat123@gmail.com>
2024-08-14 02:17:19 +0000

    cds

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,36 +1,32 @@
 #!/usr/bin/python3
-
-"""
-Query a subreddit and return the number of
-total subscribers in that subredit
-"""
-
-from requests import get
-from sys import argv
+'''A module containing functions for working with the Reddit API.
+'''
+import requests
 
 
-headers = {
-    "User-Agent": "Of course I had to use a custom User-Agent",
-    "X-Forwared-For": "iamthecavalry"
-}
+BASE_URL = 'https://www.reddit.com'
+'''Reddit's base API URL.
+'''
 
 
-def number_of_subscribers(subreddit: str) -> int:
-    """
-    Query the subreddit and return the number of
-    Active subs. If its an invalid subredit, return 0
-    """
-    response = get("https://www.reddit.com/r/{}/about.json".format(subreddit),
-                   headers=headers)
-    data = response.json()
-    try:
-        if 'error' in data.keys():
-            return 0
-        else:
-            return data['data']['subscribers']
-    except Exception as e:
-        return 0
-
-
-if __name__ == "__main__":
-    print(number_of_subscribers(argv[1]))
+def number_of_subscribers(subreddit):
+    '''Retrieves the number of subscribers in a given subreddit.
+    '''
+    api_headers = {
+        'Accept': 'application/json',
+        'User-Agent': ' '.join([
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
+            'AppleWebKit/537.36 (KHTML, like Gecko)',
+            'Chrome/97.0.4692.71',
+            'Safari/537.36',
+            'Edg/97.0.1072.62'
+        ])
+    }
+    res = requests.get(
+        '{}/r/{}/about/.json'.format(BASE_URL, subreddit),
+        headers=api_headers,
+        allow_redirects=False
+    )
+    if res.status_code == 200:
+        return res.json()['data']['subscribers']
+    return 0
