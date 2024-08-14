350f57c4ff1125321ef4aa22522748b6e2c05f82
root <www.sofyankirat123@gmail.com>
2024-08-14 02:13:52 +0000

    cdc

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,36 @@
 #!/usr/bin/python3
-""" script to obtain subscribers
-    count from a subreddit
+
 """
+Query a subreddit and return the number of
+total subscribers in that subredit
+"""
+
 from requests import get
+from sys import argv
 
 
-def number_of_subscribers(subreddit):
-    """ function to get subscriber count"""
-    if subreddit and type(subreddit) is str:
-        subscribers = 0
-        url = 'https://reddit.com/r/{}/about.json'.format(subreddit)
-        headers = {'user-agent': 'my-app/0.0.1'}
-        req = get(url, headers=headers)
-        if req.status_code == 200:
-            data = req.json()
-            subscribers = data.get('data', {}).get('subscribers', 0)
-        return subscribers
+headers = {
+    "User-Agent": "Of course I had to use a custom User-Agent",
+    "X-Forwared-For": "iamthecavalry"
+}
+
+
+def number_of_subscribers(subreddit: str) -> int:
+    """
+    Query the subreddit and return the number of
+    Active subs. If its an invalid subredit, return 0
+    """
+    response = get("https://www.reddit.com/r/{}/about.json".format(subreddit),
+                   headers=headers)
+    data = response.json()
+    try:
+        if 'error' in data.keys():
+            return 0
+        else:
+            return data['data']['subscribers']
+    except Exception as e:
+        return 0
+
+
+if __name__ == "__main__":
+    print(number_of_subscribers(argv[1]))
